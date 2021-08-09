0618a2f471f91131c68042129a107aa0970685a9
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-08-09 15:29:14 -0300

    Adicionado as tabelas cliente e funcionario no banco de dados

---
+++ LocadoraVeiculos.Database.SQLServer/dbo/Tables/TBCLIENTE.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TBCLIENTE] (
+    [Id]                INT          IDENTITY (1, 1) NOT NULL,
+    [Nome]              VARCHAR (50) NOT NULL,
+    [Endereco]          VARCHAR (50) NOT NULL,
+    [Telefone]          VARCHAR (50) NOT NULL,
+    [TipoPessoa]        VARCHAR (50) NOT NULL,
+    [Cpf]               VARCHAR (50) NULL,
+    [Cnpj]              VARCHAR (50) NULL,
+    [Rg]                VARCHAR (50) NOT NULL,
+    [DataVencimentoCNH] DATE         NOT NULL,
+    [Id_Condutor]       INT          NULL
+);
+
