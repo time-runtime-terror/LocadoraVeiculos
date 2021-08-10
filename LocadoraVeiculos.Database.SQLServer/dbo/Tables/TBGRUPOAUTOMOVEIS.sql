4652e0fbd467fa06f6e6f89dc7f11c9e611fe728
RuanMedTI <rurato2002@gmail.com>
2021-08-10 16:53:30 -0300

    tb no banco e inicio do controlador e dominio.

---
+++ LocadoraVeiculos.Database.SQLServer/dbo/Tables/TBGRUPOAUTOMOVEIS.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TBGRUPOAUTOMOVEIS]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [NomeGrupo] NCHAR(50) NOT NULL, 
+    [PlanoDiarioUm] DECIMAL(6, 2) NOT NULL, 
+    [PlanoDiarioDois] DECIMAL(6, 2) NULL, 
+    [KmControladoUm] DECIMAL(6, 2) NOT NULL, 
+    [KmControladoDois] DECIMAL(6, 2) NULL, 
+    [KmLivreUm] DECIMAL(6, 2) NOT NULL, 
+    [KmLivreDois] DECIMAL(6, 2) NULL
+)
