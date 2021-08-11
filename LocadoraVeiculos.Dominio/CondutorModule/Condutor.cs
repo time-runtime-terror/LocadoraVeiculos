ab5480f0f758e8ae805cd89feffba2295e369374
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-08-11 13:48:25 -0300

    Adicionada a classe de domínio do Condutor

--- LocadoraVeiculos.Dominio/CondutorModule/Condutor.cs
+++ LocadoraVeiculos.Dominio/CondutorModule/Condutor.cs
@@ -1,12 +1,43 @@
 ﻿using LocadoraVeiculos.Dominio.Shared;
+using System;
 
 namespace LocadoraVeiculos.Dominio.CondutorModule
 {
     public class Condutor : EntidadeBase
     {
+        public string CNH { get; }
+        public string CPF { get; }
+        public string RG { get; }
+        public DateTime VencimentoCnh { get; }
+
+        public Condutor(string cnh, string cpf, string rg, DateTime vencimentoCnh)
+        {
+            CNH = cnh;
+            CPF = cpf;
+            RG = rg;
+            VencimentoCnh = vencimentoCnh;
+        }
+
         public override string Validar()
         {
-            throw new System.NotImplementedException();
+            string resultadoValidacao = "";
+
+            if (string.IsNullOrEmpty(CNH))
+                resultadoValidacao = "O campo CNH é obrigatório";
+
+            if (string.IsNullOrEmpty(CPF))
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo CPF é obrigatório";
+
+            if (string.IsNullOrEmpty(RG))
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo RG é obrigatório";
+
+            if (VencimentoCnh == null)
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo Vencimento da CNH é obrigatório";
+
+            if (resultadoValidacao == "")
+                resultadoValidacao = "ESTA_VALIDO";
+
+            return resultadoValidacao;
         }
     }
 }
