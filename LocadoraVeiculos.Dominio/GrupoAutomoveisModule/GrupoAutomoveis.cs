1881b472b2e3fbbc8d810a0b086834246ef06ac0
RuanMedTI <rurato2002@gmail.com>
2021-08-11 15:11:52 -0300

    controlador, dominio, db e inicio dos testes do grupo de automoveis

--- LocadoraVeiculos.Dominio/GrupoAutomoveisModule/GrupoAutomoveis.cs
+++ LocadoraVeiculos.Dominio/GrupoAutomoveisModule/GrupoAutomoveis.cs
@@ -1,12 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using LocadoraVeiculos.Dominio.Shared;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LocadoraVeiculos.Dominio.GrupoAutomoveisModule
 {
-    class GrupoAutomoveis
+    public class GrupoAutomoveis : EntidadeBase
     {
+        public GrupoAutomoveis(string nomeGrupo, string planoDiarioUm, string planoDiarioDois, string kmControladoUm, 
+            string kmControladoDois, string kmLivreUm, string kmLivreDois)
+        {
+            NomeGrupo = nomeGrupo;
+            PlanoDiarioUm = planoDiarioUm;
+            PlanoDiarioDois = planoDiarioDois;
+            KmControladoUm = kmControladoUm;
+            KmControladoDois = kmControladoDois;
+            KmLivreUm = kmLivreUm;
+            KmLivreDois = kmLivreDois;
+        }
+
+        public string NomeGrupo { get; }
+
+        public string PlanoDiarioUm { get; }
+
+        public string PlanoDiarioDois { get; }
+
+        public string KmControladoUm { get; }
+
+        public string KmControladoDois { get; }
+
+        public string KmLivreUm { get; }
+
+        public string KmLivreDois { get; }
+
+        public override string Validar()
+        {
+            string resultadoValidacao = "";
+
+            if (string.IsNullOrEmpty(NomeGrupo))
+                resultadoValidacao = "O campo Nome do Grupo de Automóveis é obrigatório";
+
+            if (string.IsNullOrEmpty(PlanoDiarioUm))
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo Plano Diário é obrigatório";
+
+            if (string.IsNullOrEmpty(KmControladoUm))
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo Quilometro Controlado é obrigatório";
+
+            if (string.IsNullOrEmpty(KmLivreUm))
+                resultadoValidacao += QuebraDeLinha(resultadoValidacao) + "O campo Quilometro Livre é obrigatório";
+
+            if (resultadoValidacao == "")
+                resultadoValidacao = "ESTA_VALIDO";
+
+            return resultadoValidacao;
+        }
     }
 }
---
+++ LocadoraVeiculos.Tests/GrupoAutomoveisModule/GrupoAutomoveisTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Tests.GrupoAutomoveisModule
+{
+    [TestClass]
+    public class GrupoAutomoveisTest
+    {
+        public GrupoAutomoveisTest()
+        {
+            
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
