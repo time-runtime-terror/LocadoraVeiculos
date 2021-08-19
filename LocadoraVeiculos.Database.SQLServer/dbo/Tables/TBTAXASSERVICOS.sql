a8d0c65729edb2d3dcc3f6ebce537bfc0d0cbfcb
RuanMedTI <rurato2002@gmail.com>
2021-08-19 17:16:49 -0300

    tbtaxasservicos para testar o controlador

---
+++ LocadoraVeiculos.Database.SQLServer/dbo/Tables/TBTAXASSERVICOS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TBTAXASSERVICOS] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Servico]      VARCHAR (50)   NOT NULL,
+    [Taxa]         DECIMAL (6, 2) NOT NULL,
+    [OpcaoServico] VARCHAR (30)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
