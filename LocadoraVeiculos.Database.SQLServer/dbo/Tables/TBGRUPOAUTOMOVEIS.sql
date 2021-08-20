9c8b54d607af16dcc38c1f29ae607623ea63f32c
RuanMedTI <rurato2002@gmail.com>
2021-08-20 13:47:09 -0300

    update tabela gp automoveis

--- LocadoraVeiculos.Database.SQLServer/dbo/Tables/TBGRUPOAUTOMOVEIS.sql
+++ LocadoraVeiculos.Database.SQLServer/dbo/Tables/TBGRUPOAUTOMOVEIS.sql
@@ -1,11 +1,13 @@
-﻿CREATE TABLE [dbo].[TBGRUPOAUTOMOVEIS]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [NomeGrupo] VARCHAR(50) NOT NULL, 
-    [PlanoDiarioUm] DECIMAL(6, 2) NOT NULL, 
-    [PlanoDiarioDois] DECIMAL(6, 2) NULL, 
-    [KmControladoUm] DECIMAL(6, 2) NOT NULL, 
-    [KmControladoDois] DECIMAL(6, 2) NULL, 
-    [KmLivreUm] DECIMAL(6, 2) NOT NULL, 
-    [KmLivreDois] DECIMAL(6, 2) NULL
-)
+﻿CREATE TABLE [dbo].[TBGRUPOAUTOMOVEIS] (
+    [Id]                   INT            IDENTITY (1, 1) NOT NULL,
+    [NomeGrupo]            VARCHAR (50)   NOT NULL,
+    [PlanoDiarioUm]        DECIMAL (6, 2) NOT NULL,
+    [PlanoDiarioDois]      DECIMAL (6, 2) NULL,
+    [KmControladoUm]       DECIMAL (6, 2) NOT NULL,
+    [KmControladoDois]     DECIMAL (6, 2) NULL,
+    [KmLivreUm]            DECIMAL (6, 2) NOT NULL,
+    [KmControladoIncluido] DECIMAL (6, 2) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
