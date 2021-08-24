ca3ac3e26d80e360c383e397a4524a156e70b48f
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-08-24 11:04:48 -0300

    Iniciando o design dos forms de Locação

---
+++ LocadoraVeiculos.WindowsApp/Features/LocacaoModule/TelaCadastrarLocacaoForm.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
+{
+    partial class TelaCadastrarLocacaoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblId = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.lblVeiculo = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.lblPlano = new System.Windows.Forms.Label();
+            this.lblCaucao = new System.Windows.Forms.Label();
+            this.dateDataSaida = new System.Windows.Forms.DateTimePicker();
+            this.lblDataSaida = new System.Windows.Forms.Label();
+            this.lblDataDevolucao = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(157, 53);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(24, 18);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(187, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(327, 454);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(83, 31);
+            this.btnCancelar.TabIndex = 3;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(187, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(187, 75);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 26);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(120, 84);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(61, 18);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(187, 107);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 26);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // lblVeiculo
+            // 
+            this.lblVeiculo.AutoSize = true;
+            this.lblVeiculo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVeiculo.Location = new System.Drawing.Point(119, 115);
+            this.lblVeiculo.Name = "lblVeiculo";
+            this.lblVeiculo.Size = new System.Drawing.Size(62, 18);
+            this.lblVeiculo.TabIndex = 7;
+            this.lblVeiculo.Text = "Veículo:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(187, 139);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 26);
+            this.comboBox4.TabIndex = 10;
+            // 
+            // lblPlano
+            // 
+            this.lblPlano.AutoSize = true;
+            this.lblPlano.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlano.Location = new System.Drawing.Point(129, 146);
+            this.lblPlano.Name = "lblPlano";
+            this.lblPlano.Size = new System.Drawing.Size(52, 18);
+            this.lblPlano.TabIndex = 9;
+            this.lblPlano.Text = "Plano:";
+            // 
+            // lblCaucao
+            // 
+            this.lblCaucao.AutoSize = true;
+            this.lblCaucao.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaucao.Location = new System.Drawing.Point(114, 177);
+            this.lblCaucao.Name = "lblCaucao";
+            this.lblCaucao.Size = new System.Drawing.Size(67, 18);
+            this.lblCaucao.TabIndex = 11;
+            this.lblCaucao.Text = "Caução:";
+            // 
+            // dateDataSaida
+            // 
+            this.dateDataSaida.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateDataSaida.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateDataSaida.Location = new System.Drawing.Point(187, 203);
+            this.dateDataSaida.MinDate = new System.DateTime(2021, 8, 24, 0, 0, 0, 0);
+            this.dateDataSaida.Name = "dateDataSaida";
+            this.dateDataSaida.Size = new System.Drawing.Size(121, 26);
+            this.dateDataSaida.TabIndex = 12;
+            // 
+            // lblDataSaida
+            // 
+            this.lblDataSaida.AutoSize = true;
+            this.lblDataSaida.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataSaida.Location = new System.Drawing.Point(68, 208);
+            this.lblDataSaida.Name = "lblDataSaida";
+            this.lblDataSaida.Size = new System.Drawing.Size(113, 18);
+            this.lblDataSaida.TabIndex = 13;
+            this.lblDataSaida.Text = "Data de Saída:";
+            // 
+            // lblDataDevolucao
+            // 
+            this.lblDataDevolucao.AutoSize = true;
+            this.lblDataDevolucao.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDataDevolucao.Location = new System.Drawing.Point(35, 239);
+            this.lblDataDevolucao.Name = "lblDataDevolucao";
+            this.lblDataDevolucao.Size = new System.Drawing.Size(146, 18);
+            this.lblDataDevolucao.TabIndex = 15;
+            this.lblDataDevolucao.Text = "Data de Devolução:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(187, 235);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 26);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGravar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGravar.Location = new System.Drawing.Point(238, 454);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(83, 31);
+            this.btnGravar.TabIndex = 16;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // TelaCadastrarLocacaoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 497);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.lblDataDevolucao);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblDataSaida);
+            this.Controls.Add(this.dateDataSaida);
+            this.Controls.Add(this.lblCaucao);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.lblPlano);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.lblVeiculo);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblId);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TelaCadastrarLocacaoForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Locações";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label lblVeiculo;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label lblPlano;
+        private System.Windows.Forms.Label lblCaucao;
+        private System.Windows.Forms.DateTimePicker dateDataSaida;
+        private System.Windows.Forms.Label lblDataSaida;
+        private System.Windows.Forms.Label lblDataDevolucao;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnGravar;
+    }
+}
