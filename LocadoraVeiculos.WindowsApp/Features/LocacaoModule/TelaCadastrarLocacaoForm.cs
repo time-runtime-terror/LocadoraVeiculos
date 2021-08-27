5b5908e4a25e6a638e1a3f03885f68d781a28fbd
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-08-27 14:46:46 -0300

    Validando aluguel de veículos indisponíveis

--- LocadoraVeiculos.WindowsApp/Features/LocacaoModule/TelaCadastrarLocacaoForm.cs
+++ LocadoraVeiculos.WindowsApp/Features/LocacaoModule/TelaCadastrarLocacaoForm.cs
@@ -187,7 +187,7 @@ namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
         {
             foreach (var item in controladorLocacao.SelecionarTodos())
             {
-                if (item.Veiculo.Modelo == locacao.Veiculo.Modelo)
+                if ((item.Id != locacao.Id && item.Devolucao == "Pendente") && item.Veiculo.Placa == locacao.Veiculo.Placa)
                 {
                     Dashboard.Instancia.AtualizarRodape($"O Veículo: [{locacao.Veiculo}] não está disponível para locação!");
                     DialogResult = DialogResult.None;
