0429f4bd0cba4049ad197c65c6195bc953567555
Angelo Ribeiro Barbosa <angelorb@outlook.com.br>
2021-08-30 15:39:27 -0300

    Arrumando o tamanho da font da logo cadastro de locações

--- LocadoraVeiculos.WindowsApp/Features/LocacaoModule/TelaCadastrarLocacaoForm.Designer.cs
+++ LocadoraVeiculos.WindowsApp/Features/LocacaoModule/TelaCadastrarLocacaoForm.Designer.cs
@@ -314,59 +314,6 @@ namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(517, 60);
-            this.panel2.TabIndex = 25;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::LocadoraVeiculos.WindowsApp.Properties.Resources.sedan;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(68, 60);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(91)))), ((int)(((byte)(235)))));
-            this.label3.Location = new System.Drawing.Point(87, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(267, 34);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "LOCADORA RECH";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(358, 30);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(157, 16);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "O carro certo para você!";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(66)))));
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 60);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(98, 514);
-            this.panel3.TabIndex = 26;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(66)))));
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(539, 60);
             this.panel2.TabIndex = 24;
             // 
             // pictureBox1
@@ -384,7 +331,7 @@ namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(91)))), ((int)(((byte)(235)))));
-            this.label3.Location = new System.Drawing.Point(92, 13);
+            this.label3.Location = new System.Drawing.Point(87, 13);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(267, 34);
             this.label3.TabIndex = 6;
@@ -393,11 +340,11 @@ namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(362, 30);
+            this.label4.Location = new System.Drawing.Point(357, 31);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(178, 17);
+            this.label4.Size = new System.Drawing.Size(157, 16);
             this.label4.TabIndex = 5;
             this.label4.Text = "O carro certo para você!";
             // 
@@ -407,7 +354,7 @@ namespace LocadoraVeiculos.WindowsApp.Features.LocacaoModule
             this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel3.Location = new System.Drawing.Point(0, 60);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(98, 508);
+            this.panel3.Size = new System.Drawing.Size(98, 514);
             this.panel3.TabIndex = 25;
             // 
             // TelaCadastrarLocacaoForm
