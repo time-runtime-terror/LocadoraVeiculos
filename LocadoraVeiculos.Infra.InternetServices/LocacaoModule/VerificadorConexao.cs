1a972f443e21e65e99ef35ed577e11e9f545c893
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-15 12:27:06 -0300

    Implementando a classe VerificadorConexao

---
+++ LocadoraVeiculos.Infra.InternetServices/LocacaoModule/VerificadorConexao.cs
@@ -0,0 +1,26 @@
+﻿using LocadoraVeiculos.netCore.Dominio.LocacaoModule;
+using System.Net.NetworkInformation;
+
+namespace LocadoraVeiculos.Infra.InternetServices.LocacaoModule
+{
+    public class VerificadorConexao : IVerificadorConexao
+    {
+        public bool TemConexaoComInternet()
+        {
+            string host = "www.google.com.br";
+
+            bool result = false;
+            Ping p = new Ping();
+
+            try
+            {
+                PingReply reply = p.Send(host, 3000);
+                if (reply.Status == IPStatus.Success)
+                    return true;
+            }
+            catch { }
+
+            return result;
+        }
+    }
+}
