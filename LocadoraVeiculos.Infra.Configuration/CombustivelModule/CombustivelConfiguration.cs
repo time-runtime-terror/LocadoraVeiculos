f82901f1280c16cebb0084c40d3354c060a207ae
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-15 16:20:37 -0300

    Implementando a classe CombustivelConfiguration

---
+++ LocadoraVeiculos.Infra.Configuration/CombustivelModule/CombustivelConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using LocadoraVeiculos.netCore.Dominio.CombustivelModule;
+using System.IO;
+using System.Text.Json;
+
+namespace LocadoraVeiculos.Infra.Configuration.CombustivelModule
+{
+    public class CombustivelConfiguration
+    {
+        private Combustivel Combustivel;
+        private string ArquivoJson { get; set; }
+        private string Caminho { get; set; }
+
+        public CombustivelConfiguration()
+        {
+            Caminho = $"{Directory.GetCurrentDirectory()}\\configCombustivel.json";
+
+            if (!File.Exists(Caminho))
+                SalvarConfiguracoes(new Combustivel(0, 0, 0, 0));
+
+            ArquivoJson = File.ReadAllText(Caminho);
+        }
+
+        public string GravarCombustivel(Combustivel registro)
+        {
+            string resultadoValidacao = registro.Validar();
+
+            if (resultadoValidacao == "ESTA_VALIDO")
+                SalvarConfiguracoes(registro);
+
+            return resultadoValidacao;
+        }
+
+        public string PegarValorGasolina()
+        {
+            Combustivel = JsonSerializer.Deserialize<Combustivel>(ArquivoJson);
+            return Combustivel.Gasolina.ToString();
+        }
+
+        public string PegarValorEtanol()
+        {
+            Combustivel = JsonSerializer.Deserialize<Combustivel>(ArquivoJson);
+            return Combustivel.Etanol.ToString();
+        }
+
+        public string PegarValorDiesel()
+        {
+            Combustivel = JsonSerializer.Deserialize<Combustivel>(ArquivoJson);
+            return Combustivel.Diesel.ToString();
+        }
+
+        public string PegarValorGnv()
+        {
+            Combustivel = JsonSerializer.Deserialize<Combustivel>(ArquivoJson);
+            return Combustivel.Gnv.ToString();
+        }
+
+        private void SalvarConfiguracoes(Combustivel registro)
+        {
+            JsonSerializerOptions opcoesSerializacao
+                = new JsonSerializerOptions { WriteIndented = true };
+
+            string json = JsonSerializer.Serialize(registro, opcoesSerializacao);
+
+            File.WriteAllText(Caminho, json);
+        }
+    }
+}
