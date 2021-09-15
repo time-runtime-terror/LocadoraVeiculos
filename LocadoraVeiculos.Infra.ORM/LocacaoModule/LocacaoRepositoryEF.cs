ac86b6cf7aa86116e54acdcd73cac1d58bb09ae0
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-15 17:21:29 -0300

    Implementando a interface da classe LocacaoRepositoryEF

--- LocadoraVeiculos.Infra.ORM/LocacaoModule/LocacaoRepositoryEF.cs
+++ LocadoraVeiculos.Infra.ORM/LocacaoModule/LocacaoRepositoryEF.cs
@@ -1,12 +1,68 @@
 ﻿using LocadoraVeiculos.netCore.Dominio.LocacaoModule;
 using System;
 using System.Collections.Generic;
+using System.Data;
 
 namespace LocadoraVeiculos.Infra.ORM.LocacaoModule
 {
     public class LocacaoRepositoryEF : ILocacaoRepository
     {
-        public void InserirLocacao(Locacao locacao)
+        public Locacao ConverterEmRegistro(IDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(int id, Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InserirNovo(Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object> ObtemParametrosRegistro(Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Locacao> Pesquisar(string texto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegistrarDevolucao(Locacao registro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Locacao SelecionarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Locacao> SelecionarTodasLocacoesConcluidas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Locacao> SelecionarTodasLocacoesPendentes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Locacao> SelecionarTodos()
         {
             throw new NotImplementedException();
         }
