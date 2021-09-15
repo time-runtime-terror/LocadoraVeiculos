8800740f0e1245489cd4a0535cb8483e0eefb07e
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-15 08:46:49 -0300

    Refatorando o módulo Cliente seguindo o padrão DDD

--- LocadoraVeiculos.WindowsApp/Dashboard.cs
+++ LocadoraVeiculos.WindowsApp/Dashboard.cs
@@ -15,6 +15,9 @@ using LocadoraVeiculos.netCore.Controladores.CombustivelModule;
 using LocadoraVeiculos.WindowsApp.Features.TaxasServicosModule;
 using LocadoraVeiculos.WindowsApp.Features.LocacaoModule;
 using LocadoraVeiculos.WindowsApp.Features.DevolucaoModule;
+using LocadoraVeiculos.netCore.Dominio.ClienteModule;
+using LocadoraVeiculos.Aplicacao.ClienteModule;
+using LocadoraVeiculos.Infra.SQL.ClienteModule;
 
 namespace LocadoraVeiculos.WindowsApp
 {
@@ -76,7 +79,11 @@ namespace LocadoraVeiculos.WindowsApp
 
             AtualizarRodape(config.TipoCadastro);
 
-            operacoes = new OperacoesCliente();
+            ClienteDAO clienteRepo = new ClienteDAO();
+
+            ClienteAppService clienteService = new ClienteAppService(clienteRepo);
+
+            operacoes = new OperacoesCliente(clienteService);
 
             ConfigurarPainelRegistros();
         }
