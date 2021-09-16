0408a075e2505d3b5e52636d2a9959c60d649e58
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-16 15:52:34 -0300

    Implementando Mock Tests

---
+++ LocadoraVeiculos.Aplicacao.Tests/RegistroLocacaoTest.cs
@@ -0,0 +1,39 @@
+using LocadoraVeiculos.Aplicacao.LocacaoModule;
+using LocadoraVeiculos.netCore.Dominio.ClienteModule;
+using LocadoraVeiculos.netCore.Dominio.LocacaoModule;
+using LocadoraVeiculos.netCore.Dominio.VeiculoModule;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace LocadoraVeiculos.Aplicacao.Tests
+{
+    [TestClass]
+    public class RegistroLocacaoTest
+    {
+        byte[] foto = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+        [TestMethod]
+        public void Deve_Gerar_Recibo_Locacao()
+        {
+            // arrange
+            Cliente cliente = new Cliente("Testador 1", "testador@ndd.com", "Maria de Melo Kuster", "(49) 9805-6251", "CPF", "123123124", new DateTime(2025, 06, 30), "41421412412", "41242121412", null);
+
+            Veiculo veiculo = new Veiculo(foto, "ABC-1234", "Vectra", "Chevrolet", "Gasolina", 70, 2000, null);
+
+            Locacao locacao = new Locacao(cliente, veiculo, null, DateTime.Now, DateTime.Now.AddDays(2), 200, "Diário", null, null);
+
+            Mock<ILocacaoRepository> locacaoDaoMock = new Mock<ILocacaoRepository>();
+
+            Mock<IGeradorRecibo> geradorReciboMock = new Mock<IGeradorRecibo>();
+
+            LocacaoAppService locacaoService = new LocacaoAppService(locacaoDaoMock.Object, geradorReciboMock.Object, Mock.Of<INotificadorEmail>(), Mock.Of<IVerificadorConexao>());
+
+            // action
+            locacaoService.RegistrarNovaLocacao(locacao);
+
+            // assert
+            geradorReciboMock.Verify(x => x.GerarRecibo(It.IsAny<Locacao>()));
+        }
+    }
+}
