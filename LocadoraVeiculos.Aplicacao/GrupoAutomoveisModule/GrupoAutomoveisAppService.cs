6810d59062385882b8983dfe1ae98cc16c749b3a
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-17 08:00:37 -0300

    Herdando BaseAppService<T> em GrupoAutomoveisAppService

--- LocadoraVeiculos.Aplicacao/GrupoAutomoveisModule/GrupoAutomoveisAppService.cs
+++ LocadoraVeiculos.Aplicacao/GrupoAutomoveisModule/GrupoAutomoveisAppService.cs
@@ -1,62 +1,16 @@
-﻿using LocadoraVeiculos.netCore.Dominio.GrupoAutomoveisModule;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LocadoraVeiculos.Aplicacao.Shared;
+using LocadoraVeiculos.netCore.Dominio.GrupoAutomoveisModule;
 
 namespace LocadoraVeiculos.Aplicacao.GrupoAutomoveisModule
 {
-    public class GrupoAutomoveisAppService
+    public class GrupoAutomoveisAppService : BaseAppService<GrupoAutomoveis>
     {
         private readonly IGrupoAutomoveisRepository grupoRepository;
 
         public GrupoAutomoveisAppService(IGrupoAutomoveisRepository grupoAutomoveisRepository)
+            : base(grupoAutomoveisRepository)
         {
             this.grupoRepository = grupoAutomoveisRepository;
-        }
-
-        public string InserirNovo(GrupoAutomoveis grupoAutomoveis)
-        {
-            string resultadoValidacao = grupoAutomoveis.Validar();
-
-            if (resultadoValidacao == "ESTA_VALIDO")
-                grupoRepository.InserirNovo(grupoAutomoveis);
-
-            return resultadoValidacao;
-        }
-
-        public string Editar(int id, GrupoAutomoveis grupoAutomoveis)
-        {
-            string resultadoValidacao = grupoAutomoveis.Validar();
-
-            if (resultadoValidacao == "ESTA_VALIDO")
-                grupoRepository.Editar(id, grupoAutomoveis);
-
-            return resultadoValidacao;
-        }
-
-        public bool Excluir(int id)
-        {
-            if (grupoRepository.Excluir(id))
-                return true;
-
-            return false;
-        }
-
-        public GrupoAutomoveis SelecionarPorId(int id)
-        {
-            return grupoRepository.SelecionarPorId(id);
-        }
-
-        public List<GrupoAutomoveis> SelecionarTodos()
-        {
-            return (List<GrupoAutomoveis>)grupoRepository.SelecionarTodos();
-        }
-
-        public List<GrupoAutomoveis> Pesquisar(string texto)
-        {
-            return (List<GrupoAutomoveis>)grupoRepository.Pesquisar(texto);
         }
     }
 }
