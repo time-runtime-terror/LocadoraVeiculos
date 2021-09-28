33151197b59ec3b57d1d6a83581ae469584baee2
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-09-28 14:51:21 -0300

    Utilizando os métodos genéricos no baseAppService, no funcionarioAppService

--- LocadoraVeiculos.Aplicacao/FuncionarioModule/FuncionarioAppService.cs
+++ LocadoraVeiculos.Aplicacao/FuncionarioModule/FuncionarioAppService.cs
@@ -8,104 +8,14 @@ using System.Reflection;
 
 namespace LocadoraVeiculos.Aplicacao.FuncionarioModule
 {
-    public class FuncionarioAppService 
+    public class FuncionarioAppService : BaseAppService<Funcionario>
     {
         private readonly IFuncionarioRepository funcionarioRepository;
         
 
-        public FuncionarioAppService(IFuncionarioRepository funcionarioRepository)
+        public FuncionarioAppService(IFuncionarioRepository funcionarioRepository) : base(funcionarioRepository)
         {
             this.funcionarioRepository = funcionarioRepository;
-        }
-
-        public string InserirNovo(Funcionario registro)
-        {
-            string resultadoValidacao = registro.Validar();
-
-            if (resultadoValidacao == "ESTA_VALIDO")
-            {
-                try
-                {
-                    funcionarioRepository.InserirNovo(registro);
-                    //throw new Exception();
-                }
-                catch (Exception ex)
-                {
-                    //ver se precisa mudar o resultado da validação, pois ocorreu um erro
-                    Log.Error(ex, "Falha ao tentar registrar o funcionário");
-                }
-            }
-
-            return resultadoValidacao;
-        }
-
-        
-        public string Editar(int id, Funcionario registro)
-        {
-            string resultadoValidacao = registro.Validar();
-
-            if (resultadoValidacao == "ESTA_VALIDO")
-            {
-                try
-                {
-                    funcionarioRepository.Editar(id, registro);
-                }
-                catch (Exception ex)
-                {
-                    Log.Error(ex, "Falha ao tentar editar funcionário");
-                }
-            }
-
-           
-            return resultadoValidacao;
-        }
-
-      
-        public bool Excluir(int id)
-        {
-            try
-            {
-                if (funcionarioRepository.Excluir(id))
-                    return true;
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Falha ao tentar excluir um funcionário");
-            }
-
-            return false;
-        }
-
-        
-        public Funcionario SelecionarPorId(int id)
-        {
-            
-            try
-            {
-                return funcionarioRepository.SelecionarPorId(id);
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message);
-            }
-
-            return null;
-        }
-
-       
-        public List<Funcionario> SelecionarTodos()
-        {
-            
-            try
-            {
-                return funcionarioRepository.SelecionarTodos();
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex.Message);
-            }
-
-            return null;
         }
 
         public bool ExisteFuncionario(string usuario, string senha)
@@ -123,12 +33,5 @@ namespace LocadoraVeiculos.Aplicacao.FuncionarioModule
 
             return false;
         }
-
-        public List<Funcionario> Pesquisar(string texto)
-        {
-            throw new NotImplementedException();
-        }
-
-       
     }
 }
