61e9764ad4cb8a480b054a7b4bfa802ec191d083
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-09-29 16:08:53 -0300

    Implementando a classe LoggerExtensions

---
+++ LocadoraVeiculos.Infra.ExtensionMethods/LoggerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace LocadoraVeiculos.Infra.ExtensionMethods
+{
+    public static class LoggerExtensions
+    {
+        public static ILogger Aqui(this ILogger logger,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            return logger
+                .ForContext("MemberName", memberName)
+                .ForContext("ClassName", Path.GetFileNameWithoutExtension(sourceFilePath))
+                .ForContext("LineNumber", sourceLineNumber);
+        }
+
+        public static void FuncionalidadeUsada(this ILogger logger)
+        {
+            logger.Information("Funcionalidade {MemberName} utilizada");
+        }
+    }
+}
