3882f3d630b1759138fe0fd80d6076f782ddeb10
RuanMedTI <rurato2002@gmail.com>
2021-10-07 14:16:21 -0300

    implementacao do orm gp automoveis

--- LocadoraVeiculos.Infra.ORM/Modules/GrupoAutomoveisModule/GrupoAutomoveisRepositoryEF.cs
+++ LocadoraVeiculos.Infra.ORM/Modules/GrupoAutomoveisModule/GrupoAutomoveisRepositoryEF.cs
@@ -2,16 +2,32 @@
 using LocadoraVeiculos.netCore.Dominio.GrupoAutomoveisModule;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LocadoraVeiculos.Infra.ORM.Modules.GrupoAutomoveisModule
 {
-    public class GrupoAutomoveisRepositoryEF : BaseRepository<GrupoAutomoveis>, IGrupoAutomoveisRepositoryEntity
+    public class GrupoAutomoveisRepositoryEF : BaseRepository<GrupoAutomoveis>, IGrupoAutomoveisRepository
     {
         public GrupoAutomoveisRepositoryEF(GrupoAutomoveisDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public GrupoAutomoveis ConverterEmRegistro(IDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Existe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, object> ObtemParametrosRegistro(GrupoAutomoveis registro)
+        {
+            throw new NotImplementedException();
         }
     }
 }
