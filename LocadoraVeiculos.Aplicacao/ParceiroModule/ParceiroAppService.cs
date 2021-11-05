f093013ca4012bf9c68bd2015377eb665ff17ad5
lucasdetoffollemos <lucasdetoffollemos@gmail.com>
2021-11-05 14:39:34 -0300

    Implementando a classe ParceiroAppService

---
+++ LocadoraVeiculos.Aplicacao/ParceiroModule/ParceiroAppService.cs
@@ -0,0 +1,22 @@
+﻿using LocadoraVeiculos.Aplicacao.Shared;
+using LocadoraVeiculos.netCore.Dominio.ParceiroModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraVeiculos.Aplicacao.ParceiroModule
+{
+    public class ParceiroAppService : BaseAppService<Parceiro>
+    {
+
+        private readonly IParceiroRepository parceiroRepository;
+
+
+        public ParceiroAppService(IParceiroRepository parceiroRepository) : base(parceiroRepository)
+        {
+            this.parceiroRepository = parceiroRepository;
+        }
+    }
+}
