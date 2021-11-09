b829737301e22ee90f06222860f505e4d58b100e
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2021-11-09 14:37:42 -0300

    Adicionando migraçoes da TBCUPOM

--- LocadoraVeiculos.Infra.ORM/Migrations/20211109173633_AdicionandoTbCupom.Designer.cs
+++ LocadoraVeiculos.Infra.ORM/Migrations/20211109173633_AdicionandoTbCupom.Designer.cs
@@ -4,14 +4,16 @@ using LocadoraVeiculos.Infra.ORM;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LocadoraVeiculos.Infra.ORM.Migrations
 {
     [DbContext(typeof(LocadoraDbContext))]
-    partial class LocadoraDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211109173633_AdicionandoTbCupom")]
+    partial class AdicionandoTbCupom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LocadoraVeiculos.Infra.ORM/Migrations/20211109173633_AdicionandoTbCupom.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocadoraVeiculos.Infra.ORM.Migrations
+{
+    public partial class AdicionandoTbCupom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CupomId",
+                table: "TBLOCACAO",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TBCUPOM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,0)", nullable: false, defaultValue: 0m),
+                    DataValidade = table.Column<DateTime>(type: "date", nullable: false),
+                    ParceiroId = table.Column<int>(type: "int", nullable: true),
+                    ValorMinimo = table.Column<decimal>(type: "decimal(18,0)", nullable: false, defaultValue: 0m),
+                    Tipo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCUPOM", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TBCUPOM_TBPARCEIRO_ParceiroId",
+                        column: x => x.ParceiroId,
+                        principalTable: "TBPARCEIRO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBLOCACAO_CupomId",
+                table: "TBLOCACAO",
+                column: "CupomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBCUPOM_ParceiroId",
+                table: "TBCUPOM",
+                column: "ParceiroId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TBLOCACAO_TBCUPOM_CupomId",
+                table: "TBLOCACAO",
+                column: "CupomId",
+                principalTable: "TBCUPOM",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TBLOCACAO_TBCUPOM_CupomId",
+                table: "TBLOCACAO");
+
+            migrationBuilder.DropTable(
+                name: "TBCUPOM");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TBLOCACAO_CupomId",
+                table: "TBLOCACAO");
+
+            migrationBuilder.DropColumn(
+                name: "CupomId",
+                table: "TBLOCACAO");
+        }
+    }
+}
